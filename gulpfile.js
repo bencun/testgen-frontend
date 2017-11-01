b025e3091999e9ed22966113afee99142be1bfdf
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-01 17:31:49 +0100

    Added LESS and CSS minification to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,19 @@
 var gulp = require('gulp'),
     frep = require('gulp-frep'),
     del = require('del'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css');
+    less = require('gulp-less');
     requirejs = require('requirejs');
 
 var paths = {
-    src: 'src/**/*',
-    srcHTML: 'src/**/*.html',
-    srcCSS: 'src/**/*.css',
-    srcJS: 'src/**/*.js',
-
-    tmp: 'tmp',
-    tmpIndex: 'tmp/index.html',
-    tmpHTML: 'tmp/**/*.html',
-    tmpCSS: 'tmp/**/*.css',
-    tmpJS: 'tmp/**/*.js',
-
-    dist: 'dist',
-    distIndex: 'dist/index.html',
-    distHTML: 'dist/**/*.html',
-    distCSS: 'dist/**/*.css',
-    distJS: 'dist/**/*.js'
+    css: [
+        "tmp/vendor/bootstrap/dist/css/bootstrap.min.css",
+        "tmp/styles/**/*.css"
+    ],
+    less: [
+        'tmp/styles/less/**/*.less'
+    ]
 };
 
 //pipe all of the html files
@@ -67,7 +61,7 @@ gulp.task('buildrequire', ['buildcore'], function(){
     return gulp.src('tmp/vendor/requirejs/require.js')
     .pipe(gulp.dest('tmp/scripts/lib/'));
 });
-//replace require.js reference in HTML
+//replace the require.js reference and the Bootstrap CSS reference in HTML
 gulp.task('buildhtml', ['buildrequire'], function(){
     return gulp.src('tmp/index.html')
     .pipe(frep([{
@@ -77,6 +71,36 @@ gulp.task('buildhtml', ['buildrequire'], function(){
     .pipe(gulp.dest('tmp/'));
 });
 
-//replace vendor lib references
+//process the less files
+gulp.task('buildless', ['buildcore'], function(){
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(concat('appLESS.css'))
+        .pipe(gulp.dest('tmp/styles/'));
+});
 
-gulp.task('build', ['buildcore', 'buildrequire', 'buildhtml'], function(){});
+//minify/concat all of the css files (custom, bootstrap...) into one file
+gulp.task('buildcss', ['buildless'], function(){
+    return gulp.src(paths.css)
+        .pipe(concat('styles.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('tmp/styles/'));
+});
+
+//...delete all other css/less files
+gulp.task('deletecssless', ['buildcss'], function(){
+    del(['tmp/styles/*.css', '!tmp/styles/styles.css']);
+    del('tmp/styles/less/**/*');
+});
+
+
+//copy the necessary fontawesome files
+
+//minify js
+
+//TODO karma-jasmine unit tests
+
+//remove the vendor folder
+
+
+gulp.task('build', ['buildcore', 'buildrequire', 'buildhtml', 'buildless', 'buildcss', 'deletecssless'], function(){});
