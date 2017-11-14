d418998d7f2f45f3f7e72d05acaf8898eb770b3c
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-14 15:54:54 +0100

    Added some more fake data.

--- src/scripts/lib/factories/DataFactory.js
+++ src/scripts/lib/factories/DataFactory.js
@@ -87,72 +87,189 @@ define(['angular'], function(angular) {
             ],
             userTests:[
                 {
-                    id: 1,
                     name: "Junior full stack developer",
                     description: "This template is used during the full stack developer interview.",
                     timed: true,
                     timedTotal: true,
-                    timedTotalTime: 1,
+                    timedTotalTime: 45,
                     timedPerQuestion: true,
-                    timedPerQuestionTime: 15,
-                    questions:[
+                    timedPerQuestionTime: 60,
+                    user_id: 2,
+                    questions: [
                         {
                             id: 1,
                             question: "What are the opening and the closing tag of the PHP file?",
                             note: "There are multiple correct answers.",
+                            category: "Vanilla PHP",
+                            difficulty: 3,
                             multiselect: true,
-                            options:[
+                            options: [
                                 {
                                     option: "<!php",
-                                    correct: false
+                                    correct: false,
+                                    selected: false
                                 },
                                 {
                                     option: "<?php",
-                                    correct: false
+                                    correct: false,
+                                    selected: false
                                 },
                                 {
                                     option: "?>",
-                                    correct: false
+                                    correct: false,
+                                    selected: false
                                 },
                                 {
                                     option: ">>",
-                                    correct: false
+                                    correct: false,
+                                    selected: false
                                 }
                             ]
                         },
                         {
-                            id: 2,
-                            question: "Do global variables exist in Javascript?",
-                            note: "Select one option.",
+                            id: 37,
+                            question: "Dummy question 36",
+                            note: "Dummy question note, category 1 and difficulty 7",
+                            category: "Vanilla PHP",
+                            difficulty: 7,
                             multiselect: false,
-                            options:[
+                            options: [
                                 {
-                                    option: "Yes",
-                                    correct: false
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
                                 },
                                 {
-                                    option: "No",
-                                    correct: false
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
                                 }
                             ]
                         },
                         {
-                            id: 3,
-                            question: "How are you feeling?",
-                            note: "Select one option.",
+                            id: 25,
+                            question: "Dummy question 24",
+                            note: "Dummy question note, category 1 and difficulty 5",
+                            category: "Vanilla PHP",
+                            difficulty: 5,
                             multiselect: false,
-                            options:[
+                            options: [
                                 {
-                                    option: "Great",
-                                    correct: false
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
                                 },
                                 {
-                                    option: "Awesome",
-                                    correct: false
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
+                                }
+                            ]
+                        },
+                        {
+                            id: 5,
+                            question: "Dummy question 4",
+                            note: "Dummy question note, category 2 and difficulty 5",
+                            category: "Vanilla JS",
+                            difficulty: 5,
+                            multiselect: false,
+                            options: [
+                                {
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
+                                },
+                                {
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
+                                }
+                            ]
+                        },
+                        {
+                            id: 14,
+                            question: "Dummy question 13",
+                            note: "Dummy question note, category 2 and difficulty 4",
+                            category: "Vanilla JS",
+                            difficulty: 4,
+                            multiselect: false,
+                            options: [
+                                {
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
+                                },
+                                {
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
+                                }
+                            ]
+                        },
+                        {
+                            id: 33,
+                            question: "Dummy question 32",
+                            note: "Dummy question note, category 3 and difficulty 3",
+                            category: "Laravel 5.x",
+                            difficulty: 3,
+                            multiselect: false,
+                            options: [
+                                {
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
+                                },
+                                {
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
+                                }
+                            ]
+                        },
+                        {
+                            id: 42,
+                            question: "Dummy question 41",
+                            note: "Dummy question note, category 3 and difficulty 2",
+                            category: "Laravel 5.x",
+                            difficulty: 2,
+                            multiselect: false,
+                            options: [
+                                {
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
+                                },
+                                {
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
+                                }
+                            ]
+                        },
+                        {
+                            id: 12,
+                            question: "Dummy question 11",
+                            note: "Dummy question note, category 3 and difficulty 2",
+                            category: "Laravel 5.x",
+                            difficulty: 2,
+                            multiselect: false,
+                            options: [
+                                {
+                                    option: "Dummy option 1",
+                                    correct: false,
+                                    selected: false
+                                },
+                                {
+                                    option: "Dummy option 2",
+                                    correct: false,
+                                    selected: false
                                 }
                             ]
                         }
-                    ]
+                    ],
+                    updated_at: "2017-11-14 11:50:34",
+                    created_at: "2017-11-14 11:50:34",
+                    id: 8
                 }
             ]
         };
