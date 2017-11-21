14d8967b0bb6fbf99fc24ca4661277ebf0be1276
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-21 14:10:57 +0100

    Private methods are now exposed and therefore testable.

--- src/scripts/sections/login/controllers/LoginController.js
+++ src/scripts/sections/login/controllers/LoginController.js
@@ -4,7 +4,7 @@ define([
     angular) {
 
     var LoginController = function($rootScope, $scope, $state, AuthFactory, Notification){
-        
+        var self = this;
         $rootScope.UI.pagerVisible = false;
         $rootScope.UI.searchVisible = false;
         $rootScope.UI.navigationVisible = false;
@@ -16,61 +16,70 @@ define([
             password: ""
         };
         
-        //helper functions
-        var redirectUser = function(){
-            console.debug("Redirecting to user state...");
+        /*for testing purposes we need to expose the helper methods*/
+        self.redirectUser = redirectUser = function(){
+            console.debug("[LoginController] Redirecting to user state...");
             $rootScope.UI.goBackVisible = true;
             $rootScope.UI.logoutVisible = true;
             $state.go('app.user.userTests');
-        };
-        var redirectAdmin = function(){
-            console.debug("Redirecting to admin state...");
+        };;
+        self.redirectAdmin = function(){
+            console.debug("[LoginController] Redirecting to admin state...");
             $rootScope.UI.goBackVisible = true;
             $rootScope.UI.logoutVisible = true;
             $state.go('app.admin.tests');
-        };
+        };;
         //login the user
         $scope.tryLogin = function(useCredentials){
+            console.debug("[LoginController] Trying to login...");
             //try to login with name:pass
             if(useCredentials === true){
-                var letsTest = AuthFactory.login($scope.userData.username, $scope.userData.password).then(
+                console.debug("[LoginController] Trying to login with credentials...");
+                AuthFactory.login($scope.userData.username, $scope.userData.password).then(
                     function(data){
+                        console.debug("[LoginController] Logged in with credentials...");
                         if(data.admin === true){
-                            redirectAdmin();
+                            console.debug("[LoginController] Logged in as admin...");
+                            self.redirectAdmin();
                         }
                         else{
-                            redirectUser();
+                            console.debug("[LoginController] Logged in as user...");
+                            self.redirectUser();
                         }
                     },
                     function(data){
-                        console.log(data);
+                        console.debug("[LoginController] Login failed...");
+                        console.debug(data);
                         Notification.warning(data.message);
                     }
                 );
             }
             //else check for the existing auth
             else{
-                console.debug("Trying to login checking for auth...");
+                console.debug("[LoginController] Trying to login checking for auth...");
                 //check for existing auth
                 AuthFactory.checkAuth().then(
                     //success
                     function(response){
+                        console.debug("[LoginController] Auth check OK...");
                         if(response.permAdmin === true){
+                            console.debug("[LoginController] User has admin privileges...");
                             redirectAdmin();
                         }
                         else{
+                            console.debug("[LoginController] User has user privileges...");
                             redirectUser();
                         }
                     },
                     //failure
                     function(response){
+                        console.debug("[LoginController] Auth failed...");
                         Notification.warning('You are not logged in!');
                     }
                 );
             }
         };
         //check if user is already authenticated
-        console.debug("Trying to login...");
         $scope.tryLogin(false);
     };
     LoginController.$inject = ['$rootScope', '$scope', '$state', 'AuthFactory', 'Notification'];
